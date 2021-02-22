c4695df1d891c0b9465ff9815396d4bdd4227f32
Victorine Osewe <vosewe@powergen-re.com>
2021-02-22 20:30:48 +0300

    merge conflicts

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module CustomerOrder
 go 1.13
 
 require (
+	cloud.google.com/go v0.77.0 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/muhoro/log v0.0.0-20190227085827-9e53c05de045
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93
 )
--- main.go
+++ main.go
@@ -5,13 +5,22 @@ import (
 	"CustomerOrder/data"
 	"CustomerOrder/registering"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	// "github.com/muhoro/log"
+)
+
+var (
+	googleOauthConfig *oauth2.Config
+	// TODO: randomize it
+	oauthStateString = "pseudo-random"
 )
 
 func main() {
@@ -24,7 +33,7 @@ func main() {
 	services := initializeServices(db)
 	route := mux.NewRouter()
 	initializeRoutes(route, services)
-	log.Fatal(http.ListenAndServe(":2000", route).Error(), route)
+	http.ListenAndServe(":2000", nil)
 }
 
 type services struct {
@@ -40,6 +49,71 @@ func initializeServices(db *gorm.DB) services {
 }
 
 func initializeRoutes(route *mux.Router, s services) {
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/login", handleGoogleLogin)
+	http.HandleFunc("/callback", handleGoogleCallback)
 	route.HandleFunc("/customerorder/api/customer", api.CreateCustomer(s.registering)).Methods("POST")
 	route.HandleFunc("/customerorder/api/order", api.CreateOrder(s.registering)).Methods("POST")
+}
+
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultVal
+}
+
+func init() {
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:2000/customerorder",
+		ClientID:     "http://198363440932-tk3225nqc33voqfntlsvmdhugp2fo9tc.apps.googleusercontent.com/",
+		ClientSecret: "ytruxgpvPtqzVtaaBQWykxmj",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	var htmlIndex = `<html><body><a href="/login">Google Log In</a></body></html>`
+	fmt.Fprintf(w, htmlIndex)
+}
+
+func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL(oauthStateString)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	content, err := getUserInfo(r.FormValue("state"), r.FormValue("code"))
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "Content: %s\n", content)
+}
+
+func getUserInfo(state string, code string) ([]byte, error) {
+	if state != oauthStateString {
+		return nil, fmt.Errorf("invalid oauth state")
+	}
+
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
+
+	return contents, nil
 }
